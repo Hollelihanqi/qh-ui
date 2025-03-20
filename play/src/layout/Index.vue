10ed658f85d4d6beaaad25b87dae49ac802e3e03
YTO-20240430WWB\admin <02348618@yto.net.cn>
2025-03-20 13:22:14 +0800

    fix: 增加版本号

--- play/src/layout/Index.vue
+++ play/src/layout/Index.vue
@@ -2,7 +2,7 @@
   <div class="layout-w h-full w-full">
     <yto-layout-router type="horizaontal" class="lay-rou-container h-full">
       <template #header>
-        <yto-layout-header :collapse="collapse" @collapse="collapse = !collapse"></yto-layout-header>
+        <yto-layout-header :collapse="collapse" :userInfo="userInfo" @collapse="collapse = !collapse"></yto-layout-header>
       </template>
       <yto-layout-menu
         :menus="menus"
@@ -18,6 +18,10 @@
 import { menus } from '@/router/menu'
 
 const collapse = ref(false)
+const userInfo = {
+  userCode: '02348618',
+  userName: '魏春霈',
+}
 </script>
 
 <style lang="scss"></style>
